7c3f2ceb7caac9ebf565ff428bdad4a7d732f488
Tasiu Ismail <mynameistasiu@gmail.com>
2026-01-23 17:49:45 +0100

    Update buy-code.js

--- pages/buy-code.js
+++ pages/buy-code.js
@@ -6,7 +6,7 @@ import { saveTx } from '../utils/storage';
 import { loadTx } from "../utils/storage";
 
 
-const CODE_PRICE = 5500;
+const CODE_PRICE = 8000;
 const WA = '+2347072277091';
 
 export default function BuyCode() {
